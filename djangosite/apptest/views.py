23ccf41a5f7b92f74e81d2fe5a97d15e84880606
baijianbiao <baijianbiao@weidian.com>
2018-08-22 23:31:45 +0800

    start app

--- djangosite/apptest/views.py
+++ djangosite/apptest/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse("<h1>Welcome to my app</h1>")
+
