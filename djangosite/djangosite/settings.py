7dd97ec3d4a1acb0204b123a4351ee9657f7510f
baijianbiao <baijianbiao@weidian.com>
2018-08-23 10:21:51 +0800

    model

--- djangosite/djangosite/settings.py
+++ djangosite/djangosite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TestModel',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,17 @@ WSGI_APPLICATION = 'djangosite.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bai',
+        'USER': 'root',
+        'PASSWORD': 'bai',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
