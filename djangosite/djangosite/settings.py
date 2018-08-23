9697da838d3f9bcc472f7e22701e17c0ad3d554f
baijianbiao <baijianbiao@weidian.com>
2018-08-23 23:37:10 +0800

    model complete

--- djangosite/djangosite/settings.py
+++ djangosite/djangosite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apptest',
 ]
 
 MIDDLEWARE = [
@@ -82,7 +81,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bai',
+        'NAME': 'dj_vue',
         'USER': 'root',
         'PASSWORD': 'bai',
         'HOST': 'localhost',
