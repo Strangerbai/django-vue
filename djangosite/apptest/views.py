124c811dbf28eeba05f7bc850efab52a47da0a2f
baijianbiao <baijianbiao@weidian.com>
2018-08-27 15:07:42 +0800

    api start

--- djangosite/apptest/views.py
+++ djangosite/apptest/views.py
@@ -3,8 +3,15 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("<h1>Welcome to my app</h1>")
+    # return HttpResponse("<h1>Welcome to my app</h1>")
+    return JsonResponse({"result": 0, "msg": "执行成功"})
 
